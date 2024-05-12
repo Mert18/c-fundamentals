aa85f97ba71905be9f63f52d0065505dc3e67183
mert18 <mertuygur02@gmail.com>
2024-05-12 12:38:36 +0300

    initalized

---
+++ 1-scanf/scanf.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+int main(void) {
+	int height, length, width, volume, weight;
+
+	printf("Height: ");
+	scanf("%d", &height);
+
+	printf("Length: ");
+	scanf("%d", &length);
+
+	printf("Width: ");
+	scanf("%d", &width);
+
+
+	volume = height * length * width;
+	weight = (volume + 165) / 166;
+
+	printf("Volume (cubic inches): %d\n", volume);
+	printf("Dimensional weight (pounds): %d\n", weight);
+
+	return 0;
+}
